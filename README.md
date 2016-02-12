b8d92d436e3342ac285040cc2db792bf260d7f7e
Dmitri Shuralyov <shurcooL@gmail.com>
2016-02-11 21:07:56 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# octicons [![Build Status](https://travis-ci.org/shurcooL/octicons.svg?branch=master)](https://travis-ci.org/shurcooL/octicons) [![GoDoc](https://godoc.org/github.com/shurcooL/octicons?status.svg)](https://godoc.org/github.com/shurcooL/octicons)
+
+Package octicons provides Octicons.
+
+It's a Go package that statically embeds Octicons data, making it available via an http.FileSystem.
+
+Installation
+------------
+
+```bash
+go get -u github.com/shurcooL/octicons
+```
+
+License
+-------
+
+-	[MIT License](http://opensource.org/licenses/mit-license.php)
---
+++ doc.go
@@ -0,0 +1,10 @@
+// Package octicons provides Octicons.
+//
+// It's a Go package that statically embeds Octicons data, making it available via an http.FileSystem.
+package octicons
+
+//go:generate curl -L -O https://github.com/github/octicons/releases/download/v3.4.1/octicons.zip
+//go:generate unzip octicons.zip -d octicons
+//go:generate rm -f octicons.zip octicons/README.md octicons/octicons-local.ttf octicons/sprockets-octicons.scss
+//go:generate goexec -quiet "err := vfsgen.Generate(http.Dir(\"octicons\"), vfsgen.Options{PackageName: \"octicons\", VariableName: \"Assets\"}); if err != nil { log.Fatalln(err) }"
+//go:generate rm -rf octicons
