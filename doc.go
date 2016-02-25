ba36cc3c7d1023e63317da8a8547882dfe7676f4
Dmitri Shuralyov <shurcooL@gmail.com>
2016-02-25 02:20:06 -0800

    Update to v3.5.0.

--- doc.go
+++ doc.go
@@ -3,7 +3,7 @@
 // It's a Go package that statically embeds Octicons data, making it available via an http.FileSystem.
 package octicons
 
-//go:generate curl -L -O https://github.com/github/octicons/releases/download/v3.4.1/octicons.zip
+//go:generate curl -L -O https://github.com/github/octicons/releases/download/v3.5.0/octicons.zip
 //go:generate unzip octicons.zip -d octicons
 //go:generate rm -f octicons.zip octicons/README.md octicons/octicons-local.ttf octicons/sprockets-octicons.scss
 //go:generate goexec -quiet "err := vfsgen.Generate(http.Dir(\"octicons\"), vfsgen.Options{PackageName: \"octicons\", VariableName: \"Assets\", VariableComment: \"Assets provides Octicons data.\"}); if err != nil { log.Fatalln(err) }"
