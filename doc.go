7f0294750ad0cd13e5b395fcf939bb0c77cc073e
Dmitri Shuralyov <shurcooL@gmail.com>
2016-08-17 23:19:44 -0700

    Update to v4.3.0.
    Don't include .scss file since it seems to be needed for
    pre-processing, and it's not a final release asset for browsers to
    consume.

--- doc.go
+++ doc.go
@@ -3,7 +3,7 @@
 // It's a Go package that statically embeds GitHub Octicons data, exposing it via an http.FileSystem.
 package octicons
 
-//go:generate curl -L -o octicons.zip https://github.com/primer/octicons/archive/v4.2.0.zip
-//go:generate unzip octicons.zip octicons-4.2.0/build/font/* -d octicons
-//go:generate goexec -quiet "err := vfsgen.Generate(http.Dir(\"octicons/octicons-4.2.0/build/font\"), vfsgen.Options{PackageName: \"octicons\", VariableName: \"Assets\", VariableComment: \"Assets provides GitHub Octicons data.\"}); if err != nil { log.Fatalln(err) }"
+//go:generate curl -L -o octicons.zip https://github.com/primer/octicons/archive/v4.3.0.zip
+//go:generate unzip octicons.zip octicons-4.3.0/build/font/octicons.* -d octicons
+//go:generate goexec -quiet "err := vfsgen.Generate(http.Dir(\"octicons/octicons-4.3.0/build/font\"), vfsgen.Options{PackageName: \"octicons\", VariableName: \"Assets\", VariableComment: \"Assets provides GitHub Octicons data.\"}); if err != nil { log.Fatalln(err) }"
 //go:generate rm -rf octicons.zip octicons
